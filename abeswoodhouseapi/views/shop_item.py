9ecaa1f46dc523dd63d0d942e3de84f0d7bf278a
turner lynch <turnerjoylynch@gmail.com>
2022-08-23 19:42:13 -0700

    methods update

--- abeswoodhouseapi/views/shop_item.py
+++ abeswoodhouseapi/views/shop_item.py
@@ -1,4 +1,6 @@
 """ A module for handling Shop Items requests """
+from cgi import print_exception
+from turtle import title
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
@@ -23,6 +25,41 @@ class ShopItemView(ViewSet):
         shop_items = ShopItem.objects.all()
         serializer = ShopItemSerializer(shop_items, many=True)
         return Response(serializer.data)
+    
+    def create(self, request):
+        """ Handle a POST request for a shop item """
+        # incoming_user = request.auth.user (I have a question about this)
+        new_shop_item = ShopItem.objects.create(
+            title=request.data["title"],
+            description=request.data["description"],
+            price=request.data["price"],
+            type=request.data["type"],
+        )
+        serializer = ShopItemSerializer(new_shop_item)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    def update(self, request, pk):
+        """ Handles a PUT request for a shop item """
+        editing_shop_item = ShopItem.objects.get(pk=pk)
+        
+        editing_shop_item.title = request.data["title"]
+        editing_shop_item.description = request.data["description"]
+        editing_shop_item.price = request.data["price"]
+        editing_shop_item.type = request.data["type"]
+        
+        editing_shop_item.save()
+        
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+    
+    def destroy(self, request, pk):
+        """ Handles a DELETE request for a shop item """
+        try:
+            shop_item = ShopItem.objects.get(pk=pk)
+            shop_item.delete()
+        except ShopItem.DoesNotExist as e:
+            return Response(None, status=status.HTTP_404_NOT_FOUND)
+        
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
 
 class ShopItemSerializer(serializers.ModelSerializer):
     """ JSON serializer for shop items """
--- abeswoodshop/urls.py
+++ abeswoodshop/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from abeswoodhouseapi.views import ShopItemView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'shopitems', ShopItemView, 'shopitem')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
